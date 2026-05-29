956ba65be245666d5b6460d8a47c83a7a709a17d
Nazif-12 <S4144302@student.rmit.edu.au>
2026-05-29 19:52:58 +1000

    jj

--- intelligent-bus-driver-guidance-system/src/test/java/com/example/AppTest.java
+++ intelligent-bus-driver-guidance-system/src/test/java/com/example/AppTest.java
@@ -1,9 +1,7 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit test for simple App.
  */
